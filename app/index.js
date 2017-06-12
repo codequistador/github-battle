70a07bbeb270973bf911006fe515fc9bf38389d5
Justin Daining <jdaining@instructure.com>
2017-06-11 20:15:46 -0600

    proptypes

--- app/index.js
+++ app/index.js
@@ -1,6 +1,29 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
 require('./index.css');
+
+class Badge extends React.Component {
+  render() {
+    return (
+      <div>
+        <img 
+          src={this.props.img}
+          alt='Avatar'
+          style={{width: 100, height: 100}}
+        />
+        <h1>Name: {this.props.name}</h1>
+        <h3>Username: {this.props.username}</h3>
+      </div>
+    )
+  }
+}
+
+Badge.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+}
 
 class App extends React.Component {
   render() {
@@ -11,6 +34,10 @@ class App extends React.Component {
 }
 
 ReactDOM.render(
-  <App />,
+  <Badge
+    name='Justin Daining'
+    username='codequistador'
+    img='https://avatars0.githubusercontent.com/u/8783485?v=3&s=460'
+  />,
   document.getElementById('app')
 );
