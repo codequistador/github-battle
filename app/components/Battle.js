6f93791ccc7fcd3ffd7c2ee93dc7cc85f584d142
Justin Daining <jdaining@instructure.com>
2017-07-02 16:10:36 -0600

    router

---
+++ app/components/Battle.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class Battle extends React.Component {
+  render() {
+    return (
+      <div>
+        BATTLE MOTHER FUCKERS!
+      </div>
+    )
+  }
+}
+
+module.exports = Battle;
